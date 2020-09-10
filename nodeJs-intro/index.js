df1e7523a8b8828556247b8079247c3c08252dd4
Boris Jancic <jancicboris56@gmail.com>
2020-09-10 15:12:52 +0200

    When run by node it prints 1 superhero and 1 villan name

--- nodeJs-intro/index.js
+++ nodeJs-intro/index.js
@@ -1,5 +1,8 @@
 var superheroes = require('superheroes');
+const supervillains = require('supervillains');
 
 var mySuperHeroName = superheroes.random();
+var mySyperVillan = supervillains.random();
 
 console.log(mySuperHeroName);
+console.log(mySyperVillan);
