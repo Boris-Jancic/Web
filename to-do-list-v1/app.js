dcb140740b197a88dba95e16fa248c0081d0ebb0
Boris Jancic <jancicboris56@gmail.com>
2020-09-19 19:53:50 +0200

    Added work and about post routes

--- to-do-list-v1/app.js
+++ to-do-list-v1/app.js
@@ -1,38 +1,43 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const date = require(__dirname + "/date.js");
 
-var items = ["Foo", "Foo", "Foo"];  // List that holds all to-do items
+let items = ["Get up", "Go to work", "Buy groceries"];  // List that holds all to-do items
+let workItems = []
 
 app.set('view engine', 'ejs'); // Tells app to use ejs as a view engine
 app.use(bodyParser.urlencoded({encoded: true}));  // Parses through body
-
+app.use(express.static("public"));
 
 app.get("/", function (req, res) {
+  let day = date.getDate(); 
 
-  var today = new Date();
-
-  var options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-  };
-
-  var day = today.toLocaleDateString("en-US", options); // Generates a string from the current date
-
-  res.render("list", {kindOfDay: day, newListItems: items});  // Passes the current day and list items to website
+  res.render("list", {listTitle: day, newListItems: items});  // Passes the current day and list items to website
 });
 
 
 app.post("/", function (req, res) {
+  let item = req.body.newItem;
 
-  var item = req.body.newItem;
+  if (req.body.list === "Work") {
+    workItems.push(item);   //  Item is appended to items when the user clicks submit
+    res.redirect("/work");   // Redirects to app.get (work)
+  } else {
+    items.push(item); //  Item is appended to items when the user clicks submit
+    res.redirect("/");  // Redirects to app.get
+  }
 
-  items.push(item); //  Item is appended to items when the user clicks submit
-
-  res.redirect("/");  // Redirects to app.get
 })
 
+
+app.get("/work", function (req, res) {
+  res.render("list", {listTitle: "Work List", newListItems: workItems});
+})
+
+app.get("/about", function (req, res) {
+  res.render("about");
+})
 
 app.listen(3000, function () {
 
