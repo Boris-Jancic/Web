481554c087186ec907d11d5a9ca7a442a30e8ba2
Boris Jancic <jancicboris56@gmail.com>
2020-09-23 17:32:46 +0200

    Implemented a array that holds all posts

--- Blog-challange/app.js
+++ Blog-challange/app.js
@@ -9,7 +9,7 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
-let newPost;
+let posts = [];
 
 app.set('view engine', 'ejs');
 
@@ -17,9 +17,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
-
 app.get("/", function (req, res) {
-  res.render("home", {paragraphText: homeStartingContent});
+  res.render("home", {paragraphText: homeStartingContent, posts: posts});
 })
 
 app.get("/about", function (req, res) {
@@ -35,17 +34,21 @@ app.get("/compose", function (req, res) {
   res.render("compose");
 })
 
+app.get("/:post", function (req, res) {
+  console.log(req.params.post);
+})
+
+
 app.post("/compose", function (req, res) {
-
-
-  let post = {
+  const post = {
     title: req.body.postTitle,
     text: req.body.postText
   };
 
-  console.log(post);
-})
+  posts.push(post);
 
+  res.redirect("/")
+})
 
 
 app.listen(3000, function() {
